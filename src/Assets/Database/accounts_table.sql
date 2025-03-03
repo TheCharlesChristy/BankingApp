4aa96636e8fe770a1769d0759b57b453ba9fe988
TheCharlesChristy <charleschristy325@gmail.com>
2025-03-03 13:15:44 +0000

    Added interest rates

--- src/Assets/Database/accounts_table.sql
+++ src/Assets/Database/accounts_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS Accounts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     balance REAL NOT NULL,
+    interest_rate REAL NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
--- src/Structs/Accounts.java
+++ src/Structs/Accounts.java
@@ -6,19 +6,22 @@ public class Accounts {
     public int id;
     public int user_id;
     public float balance;
+    public float interest_rate;
     public LocalDateTime created_at;
 
-    public Accounts(int id, int user_id, float balance, LocalDateTime created_at) {
+    public Accounts(int id, int user_id, float balance, float interest_rate, LocalDateTime created_at) {
         this.id = id;
         this.user_id = user_id;
         this.balance = balance;
+        this.interest_rate = interest_rate;
         this.created_at = created_at;
     }
 
-    public Accounts(int id, int user_id, int balance, LocalDateTime created_at) {
+    public Accounts(int id, int user_id, int balance, float interest_rate, LocalDateTime created_at) {
         this.id = id;
         this.user_id = user_id;
         this.balance = (float)balance;
+        this.interest_rate = interest_rate;
         this.created_at = created_at;
     }
 
