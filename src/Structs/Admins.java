baf31a859a737ec228d2aa7fe3a43296217ddf1e
TheCharlesChristy <charleschristy325@gmail.com>
2025-03-03 12:59:40 +0000

    Admins Struct

--- src/Structs/Admins.java
+++ src/Structs/Admins.java
@@ -1,5 +1,11 @@
 package src.Structs;
 
 public class Admins {
-    
+    public int id;
+    public String username;
+
+    public Admins(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
 }
