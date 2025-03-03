d34f217d32f777724c92c6bb9e7e633b8d350876
TheCharlesChristy <charleschristy325@gmail.com>
2025-03-03 13:01:42 +0000

    Transactions Struct

--- src/Structs/Transactions.java
+++ src/Structs/Transactions.java
@@ -1,5 +1,35 @@
 package src.Structs;
 
+import java.time.LocalDateTime;
+
 public class Transactions {
+    public int id;
+    public int account_id;
+    public float amount;
+    public String type;
+    public LocalDateTime created_at;
+
+    public Transactions(int id, int account_id, float amount, String type, LocalDateTime created_at) {
+        this.id = id;
+        this.account_id = account_id;
+        this.amount = amount;
+        this.type = type;
+        this.created_at = created_at;
+    }
+
+    public Transactions(int id, int account_id, float amount, String type) {
+        this(id, account_id, amount, type, LocalDateTime.now());
+    }
+
+    public Transactions(int account_id, float amount, String type, LocalDateTime created_at) {
+        this.account_id = account_id;
+        this.amount = amount;
+        this.type = type;
+        this.created_at = created_at;
+    }
+
+    public Transactions(int account_id, float amount, String type) {
+        this(account_id, amount, type, LocalDateTime.now());
+    }
     
 }
