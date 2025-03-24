69dc2effeb57ce75836c99fa31e06020ff0d71db
TheCharlesChristy <charleschristy325@gmail.com>
2025-03-24 13:58:56 +0000

    added registration input validation and create adminaccount manager console interface

---
+++ src/Server/CommandLine/Components/AdminAccountManager.java
@@ -0,0 +1,39 @@
+package src.Server.CommandLine.Components;
+
+import src.Server.DataBaseInterface;
+import src.Server.CommandLine.CommandLineFunctions;
+
+
+public class AdminAccountManager extends CommandLineFunctions {
+    
+    public AdminAccountManager(DataBaseInterface db_interface) {
+        super(db_interface);
+    }
+
+    public int run() {
+        return prompt_account_manager();
+    }
+
+    private boolean verify_input(String input) {
+        if (input.equals("1") || input.equals("2") || input.equals("3") || input.equals("4") || input.equals("5")) {
+            return true;
+        }
+        return false;
+    }
+
+    public int prompt_account_manager() {
+        get_prompt_print("AccountManager");
+        int incorrect_attempts = 0;
+        while (incorrect_attempts < 3) {
+            String choice = get_prompt_enter("AccountManagerChoice");
+            if (!verify_input(choice)) {
+                io.println("Invalid input");
+                incorrect_attempts++;
+                continue;
+            }else{
+                return Integer.parseInt(choice);
+            }
+        }
+        return 5; // logout
+    }
+}
--- src/Server/CommandLine/Components/Register.java
+++ src/Server/CommandLine/Components/Register.java
@@ -16,6 +16,46 @@ public class Register extends CommandLineFunctions {
         prompt_register();
     }
 
+    public boolean verify_password_req(String password) {
+        // Password must be longer than 8 chars and contain a capital letter and a number
+        if (password.length() < 8) {
+            io.println("Password must be at least 8 characters long");
+            return false;
+        }
+        if (!password.matches(".*[A-Z].*")) {
+            io.println("Password must contain at least one capital letter");
+            return false;
+        }
+        if (!password.matches(".*[a-z].*")) {
+            io.println("Password must contain at least one lowercase letter");
+            return false;
+        }
+        if (!password.matches(".*[0-9].*")) {
+            io.println("Password must contain at least one number");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean verify_email(String email) {
+        if (!email.contains("@")) {
+            io.println("Invalid email");
+            return false;
+        }
+        if (!email.contains(".")) {
+            io.println("Invalid email");
+            return false;
+        }
+        // Split the email at the last '.' and check if the last part is less than 2 characters
+        String[] split_email = email.split("\\.");
+        if (split_email[split_email.length - 1].length() < 2) {
+            io.println("Invalid email");
+            return false;
+        }
+
+        return true;
+    }
+
     public void prompt_register() {
         get_prompt_print("Register");
         String username = get_prompt_enter("RegisterUsername");
@@ -27,8 +67,18 @@ public class Register extends CommandLineFunctions {
         }
 
         // Create user if they do not exist
-        String password = get_prompt_enter_pass("RegisterPassword");
+        int counter = 0;
+        String password = "";
+        get_prompt_print("RegisterPasswordReq");
+        while (counter < 3) {
+            password = get_prompt_enter_pass("RegisterPassword");
+            if (verify_password_req(password)) {
+                break;
+            }
+            counter++;
+        }
         String confirmed_password = get_prompt_enter_pass("RegisterPasswordConfirm");
+
         if (password.equals(confirmed_password)) {
             io.println("User created successfully");
             String email = get_prompt_enter("RegisterEmail");
